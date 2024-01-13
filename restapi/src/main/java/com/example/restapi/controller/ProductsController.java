6eb091f594af37f1431375f38ef8f601f7a52ef1
Akhilesh K M <kmakhil294@gmail.com>
2024-01-13 20:41:59 +0530

    Handling exception, and using services

--- restapi/src/main/java/com/example/restapi/controller/ProductsController.java
+++ restapi/src/main/java/com/example/restapi/controller/ProductsController.java
@@ -2,6 +2,7 @@ package com.example.restapi.controller;
 
 import com.example.restapi.model.Product;
 import com.example.restapi.repository.ProductRepository;
+import com.example.restapi.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,53 +12,33 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api/products/")
 public class ProductsController {
 
-    private ProductRepository productRepository;
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
 
     private Logger LOG = LoggerFactory.getLogger(ProductsController.class);
 
-    @Autowired
-    public void productRepository(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
     @GetMapping("{id}")
     public Product getProduct(@PathVariable(name = "id") String id) {
-        return productRepository.findById(id).orElse(null);
+        return productService.getProduct(id);
     }
 
     @PostMapping("")
     public Product saveProduct(@RequestBody Product product) {
-
-        return productRepository.save(product);
+        return productService.createProduct(product);
     }
 
     @PutMapping("{id}")
     public Product updateProduct(@RequestBody Product productToUpdate, @PathVariable(name = "id") String id) {
-        Product foundProduct = productRepository.findById(id).orElse(null);
-        if(foundProduct != null) {
-            foundProduct.setName(productToUpdate.getName());
-            foundProduct.setDescription(productToUpdate.getDescription());
-            foundProduct.setType(productToUpdate.getType());
-            foundProduct.setCategory(productToUpdate.getCategory());
-
-
-            return productRepository.save(foundProduct);
-
-
-        } else {
-            LOG.info("NO product with id");
-            return productToUpdate;
-        }
-
+        return productService.updateProduct(productToUpdate, id);
     }
 
     @DeleteMapping("{id}")
     public void deleteProduct(@PathVariable(name = "id") String id) {
-        Product foundProduct = productRepository.findById(id).orElse(null);
-
-        if(foundProduct != null) {
-            productRepository.deleteById(id);
-        }
+        productService.deleteProduct(id);
     }
 
 }
---
+++ restapi/src/main/java/com/example/restapi/dto/MessageDTO.java
@@ -0,0 +1,25 @@
+package com.example.restapi.dto;
+
+public class MessageDTO {
+    public MessageDTO() {
+    }
+
+    private String message;
+    private String type;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
