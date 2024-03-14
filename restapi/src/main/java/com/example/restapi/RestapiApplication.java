746210fd0668a7206167667824ed3b006d607006
Akhilesh K M <kmakhil294@gmail.com>
2024-03-14 20:18:50 +0530

    code modification

--- restapi/src/main/java/com/example/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/example/restapi/RestapiApplication.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class RestapiApplication implements CommandLineRunner {
 
 	private ProductRepository productRepository;
