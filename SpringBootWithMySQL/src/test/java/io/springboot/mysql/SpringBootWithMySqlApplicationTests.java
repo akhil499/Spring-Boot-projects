2334fff30f8d106144740f81ba4e914a4108058a
Akhilesh K M <kmakhil294@gmai.com>
2024-07-05 22:07:35 +0530

    springboot project with MySQL database, initial commit

---
+++ SpringBootWithMySQL/src/test/java/io/springboot/mysql/SpringBootWithMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springboot.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
