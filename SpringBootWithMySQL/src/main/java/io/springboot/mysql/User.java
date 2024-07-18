4012338b012e6268bd71de4f3d1e5c6fcdbc0532
Akhilesh K M <kmakhil294@gmai.com>
2024-07-18 20:07:41 +0530

    modified the setup to work with h2 database and added dockerfile to containerize the application

--- SpringBootWithMySQL/src/main/java/io/springboot/mysql/User.java
+++ SpringBootWithMySQL/src/main/java/io/springboot/mysql/User.java
@@ -1,11 +1,9 @@
 package io.springboot.mysql;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "user_table")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
